5f0af05e7e2443cae598ce3cefb940111fe2e8b0
skhub12 <shruti.iisc@gmail.com>
2021-12-07 03:04:51 +0100

    Project and resources

---
+++ Session_3/if_condition_examples.sh
@@ -0,0 +1,16 @@
+a=100
+if [[ $a -eq 10 ]];then
+	echo "1:True"
+else
+	echo "1:False"
+fi
+
+#-----------------------------------------------------------------------------------
+#make it True by changing the value of a variable
+#a=10
+#if [[ $a -eq 10 ]];then
+#	echo "1:True"
+#else
+#	echo "1:False"
+#fi
+
---
+++ Session_3/loop_simple_examples.sh
@@ -0,0 +1,14 @@
+echo "Loop 1:......................................."
+ for x in 1 2 3 4 5;do
+	echo "For loop 1 : $x"
+done
+
+echo "Loop 2:......................................."
+ for name in a b shruti yourname sirname;do
+        echo "For loop 2 : $name"
+done
+
+echo "Loop 3:......................................."
+ for x in $(seq 100);do
+        echo "For loop 3 : $x"
+done
